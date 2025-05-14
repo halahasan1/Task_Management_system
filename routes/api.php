2e37722f795a07884bfa50334ef16cc4ef44fbd4
halahasan1 <hala.hasan.hh399@gmail.com>
2025-05-14 18:13:29 +0300

    add Task , Status and Comment Routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\StatusController;
+use App\Http\Controllers\Api\CommentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +15,30 @@ Route::get('/user', function (Request $request) {
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 Route::middleware('auth:sanctum')->post('/logout',[AuthController::class,'logout']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // Task Routes
+    Route::prefix('tasks')->group(function () {
+        Route::get('/', [TaskController::class, 'index']); 
+        Route::post('/', [TaskController::class, 'store']);
+        Route::get('{task}', [TaskController::class, 'show']);
+        Route::put('{task}', [TaskController::class, 'update']);
+        Route::delete('{task}', [TaskController::class, 'destroy']);
+    });
+
+    // Status Routes
+    Route::prefix('statuses')->middleware('permission:manage-statuses')->group(function () {
+        Route::get('/', [StatusController::class, 'index']);
+        Route::post('/', [StatusController::class, 'store']);
+        Route::put('{status}', [StatusController::class, 'update']);
+        Route::delete('{status}', [StatusController::class, 'destroy']);
+    });
+
+    // Comment Routes
+    Route::prefix('comments')->group(function () {
+        Route::post('/', [CommentController::class, 'store']);
+        Route::put('{comment}', [CommentController::class, 'update']);
+        Route::delete('{comment}', [CommentController::class, 'destroy']);
+    });
+});
